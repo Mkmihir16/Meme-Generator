856dc3de94d7225150a148bb6a388732635b7ae6
Mkmihir16 <132197042+Mkmihir16@users.noreply.github.com>
2024-02-04 22:26:24 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let img=document.querySelector(".img img")
+let obj;
+let repeat;
+let fun=async function memegenerate(){
+    let url=await fetch("https://meme-api.com/gimme");
+    console.log("It is url",url)
+    obj= await url.json();
+    console.log(obj.url);
+    img.src=`${obj.url}`
+} 
+fun();
+// img.addEventListener("mouseleave",()=>{
+//      repeat=setInterval(fun,1000);
+// })
+// img.addEventListener("mouseover",()=>{
+//     clearInterval(repeat);
+// })
